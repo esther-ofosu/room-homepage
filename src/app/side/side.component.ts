386ac6f5f540632927d411d88bc00ba10c16bc5a
Esther Ofosu <esther.ofosu@amalitech.com>
2024-04-10 16:42:17 +0000

    responsive(fitting the screen)

--- src/app/side/side.component.ts
+++ src/app/side/side.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-side',
@@ -8,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrl: './side.component.css'
 })
 export class SideComponent {
+  desktopUrls: string[] = [  
+    '/assets/images/desktop-image-hero-1.jpg',
+    '/assets/images/desktop-image-hero-2.jpg',
+    '/assets/images/desktop-image-hero-3.jpg',
+  ];
 
+  mobileUrls: string[] = [
+    '/assets/images/mobile-image-hero-1.jpg',
+    '/assets/images/mobile-image-hero-2.jpg',
+    '/assets/images/mobile-image-hero-3.jpg',
+  ];
+
+  currentImageIndex = 0;  
+
+  forward() {
+    const backgImages= document.getElementById('backg') as HTMLDivElement
+    this.currentImageIndex++; 
+    console.log("picture changed successfully")
+    if (this.currentImageIndex >= this.desktopUrls.length) {
+      this.currentImageIndex = 0;  
+    }
+    console.log("picture changed successfully2")
+
+    const screenSize= window.matchMedia('(max-width:768px)')
+    if ('max-width:768px'){
+      backgImages.style.backgroundImage = `url(${this.mobileUrls[this.currentImageIndex]})`
+    }
+
+    else{
+      backgImages.style.backgroundImage = `url(${this.desktopUrls[this.currentImageIndex]})`
+    }
+
+  }
+
+  backward() {
+    const backgImages= document.getElementById('backg') as HTMLDivElement
+    this.currentImageIndex--;
+    console.log("backward triggered")
+    if (this.currentImageIndex < 0) {
+      this.currentImageIndex = this.desktopUrls.length - 1; 
+    }
+
+    const screenSize= window.matchMedia('(max-width:768px)')
+    if ('max-width:768px'){
+      backgImages.style.backgroundImage = `url(${this.mobileUrls[this.currentImageIndex]})`
+    }
+
+    else{
+      backgImages.style.backgroundImage = `url(${this.desktopUrls[this.currentImageIndex]})`
+    }
+
+  }
+
+  getCurrentImageUrl(): string {
+    return this.desktopUrls[this.currentImageIndex];  // Get the URL of the current image
+  }
 }
+
+
